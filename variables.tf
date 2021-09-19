985947e85becd2df43f5431ab706416fa1083618
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-09-19 18:58:06 +0530

    Added additional tag

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "instance_name" {
   type        = string
   default     = "ExampleAppServerInstance"
 }
+
+variable "environment_name"{
+  description="Value of the Environment tag for the EC2 instance"
+  type = string
+  default="dev"
+}
